094fae0a9e8e339875a948ddcdb6c8426198a7f4
Your Name <you@example.com>
2025-01-23 19:58:01 +0300

    Added db test.

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """test the calc module"""
 
@@ -14,9 +15,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """test subtracting numbers"""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
